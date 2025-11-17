06a64077fb02a512c8d27677db28896c33538f93
AryandaruazzamTruestudio <enthusiasthandsometech@gmail.com>
2025-11-18 05:42:17 +0700

    Update script.js

--- ai-chat/script.js
+++ ai-chat/script.js
@@ -8,7 +8,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
-const API_KEY = "#####";
+const API_KEY = "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";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
